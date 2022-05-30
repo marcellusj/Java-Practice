922968c47c293f492be55ce849c45e611c36aefd
Marcellus <majordanduke22@gmail.com>
2022-05-30 11:09:26 -0700

    Practice using Implicit and Explicit Casting.

--- JavaCoding/src/Main.java
+++ JavaCoding/src/Main.java
@@ -1,12 +1,12 @@
-import java.awt.*;
-import java.util.Date;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        Point point1 = new Point(1, 1);
-        Point point2 = point1;
-        point1.x = 2;
-        System.out.println(point2);
+        // Implicit casting
+        // byte > short > int > long > float > double
+        String x = "1";
+        int y = Integer.parseInt(x) + 2;
+        System.out.println(y);
     }
 }

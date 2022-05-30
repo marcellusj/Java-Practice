24a8efaa0ddd254c65cb7b89ef5700ff7812ee75
Marcellus <majordanduke22@gmail.com>
2022-05-29 17:41:27 -0700

    Example Reference Type

--- JavaCoding/src/Main.java
+++ JavaCoding/src/Main.java
@@ -1,10 +1,10 @@
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
         byte age = 30;
-        long viewsCount = 3_123_456_789L;
-        float price = 10.99f;
-        char letter = 'A';
-        boolean isEligible = false;
+        Date now = new Date();
+        System.out.println(now);
     }
 }

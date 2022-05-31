f06393ac1c9cef20e7267977323235549f5f7218
Marcellus <majordanduke22@gmail.com>
2022-05-30 17:53:11 -0700

    Completed Mortgage Calculator Project.

--- JavaCoding/src/Main.java
+++ JavaCoding/src/Main.java
@@ -5,9 +5,30 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Name: ");
-        String name = scanner.nextLine().trim();
-        System.out.println("You are " + name);
+        // Principal
+        System.out.print("Principal: ");
+        int principal = scanner.nextInt();
+
+        // Annual Interest Rate
+        System.out.print("Annual Interest Rate: ");
+        float annualInterest = scanner.nextFloat();
+        float monthlyIR = annualInterest / PERCENT / MONTHS_IN_YEAR;
+
+        // Period in Years
+        System.out.print("Period (Years): ");
+        byte years = scanner.nextByte();
+        int numberOfPayments = years * MONTHS_IN_YEAR;
+
+        double mortgage = principal
+                * (monthlyIR * Math.pow(1 + monthlyIR, numberOfPayments))
+                / (Math.pow(1 + monthlyIR, numberOfPayments) - 1);
+
+        String mortgageCurrency = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageCurrency);
+
     }
 }

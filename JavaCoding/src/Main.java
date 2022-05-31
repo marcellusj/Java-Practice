1f61613dd5c51d549a7c7e7b4994707429094d00
Marcellus <majordanduke22@gmail.com>
2022-05-30 17:22:06 -0700

    Example of Reading input.

--- JavaCoding/src/Main.java
+++ JavaCoding/src/Main.java
@@ -1,10 +1,13 @@
 import java.text.NumberFormat;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        String result = NumberFormat.getPercentInstance().format(0.1);
-        System.out.println(result);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Name: ");
+        String name = scanner.nextLine().trim();
+        System.out.println("You are " + name);
     }
 }

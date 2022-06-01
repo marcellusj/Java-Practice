93578989f20516d45cdb809ae944bb09b938454c
Marcellus <majordanduke22@gmail.com>
2022-06-01 00:27:17 -0700

    If Statement Examples.

--- JavaCoding/src/Main.java
+++ JavaCoding/src/Main.java
@@ -5,30 +5,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
+        int temp = 32;
 
-        Scanner scanner = new Scanner(System.in);
-        // Principal
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
-
-        // Annual Interest Rate
-        System.out.print("Annual Interest Rate: ");
-        float annualInterest = scanner.nextFloat();
-        float monthlyIR = annualInterest / PERCENT / MONTHS_IN_YEAR;
-
-        // Period in Years
-        System.out.print("Period (Years): ");
-        byte years = scanner.nextByte();
-        int numberOfPayments = years * MONTHS_IN_YEAR;
-
-        double mortgage = principal
-                * (monthlyIR * Math.pow(1 + monthlyIR, numberOfPayments))
-                / (Math.pow(1 + monthlyIR, numberOfPayments) - 1);
-
-        String mortgageCurrency = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println("Mortgage: " + mortgageCurrency);
-
+        if (temp > 30) {
+            System.out.println("It's a hot day");
+            System.out.println("Drink water");
+        }
+        else if (temp > 20)
+            System.out.println("Beautiful day");
+        else
+            System.out.println("Cold day");
     }
 }

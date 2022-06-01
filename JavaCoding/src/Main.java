e104e6423e743a18a28d0ec1eadfee897c7d50ce
Marcellus <majordanduke22@gmail.com>
2022-06-01 00:34:21 -0700

    Ternary Operator Example

--- JavaCoding/src/Main.java
+++ JavaCoding/src/Main.java
@@ -5,15 +5,9 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int temp = 32;
+        int income = 120_000;
+        String className = income > 100_000 ? "First" : "Economy";
+        System.out.println(className);
 
-        if (temp > 30) {
-            System.out.println("It's a hot day");
-            System.out.println("Drink water");
-        }
-        else if (temp > 20)
-            System.out.println("Beautiful day");
-        else
-            System.out.println("Cold day");
     }
 }

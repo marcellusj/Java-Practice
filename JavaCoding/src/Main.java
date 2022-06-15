c11e1c12c0aec9ac5d75aa68c5b09b8e3acca95c
Marcellus <majordanduke22@gmail.com>
2022-06-15 07:54:24 -0700

    Creating a method

--- JavaCoding/src/Main.java
+++ JavaCoding/src/Main.java
@@ -4,51 +4,12 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
+        String greeting = greetUser("Marcellus", "Jordan");
+        System.out.println(greeting);
 
-        int principal = 0;
-        float monthlyIR = 0;
-        int numberOfPayments = 0;
+    }
 
-        Scanner scanner = new Scanner(System.in);
-        // Principal
-        while (true) {
-            System.out.print("Principal ($1K - $1M): ");
-            principal = scanner.nextInt();
-            if (principal >= 1000 && principal <= 1_000_000)
-                break;
-            System.out.println("Enter a value between 1000 and 1000000.");
-        }
-
-        // Annual Interest Rate
-        while (true) {
-            System.out.print("Annual Interest Rate: ");
-            float annualInterest = scanner.nextFloat();
-            if (annualInterest >= 1 && annualInterest <=30) {
-                monthlyIR = annualInterest / PERCENT / MONTHS_IN_YEAR;
-                break;
-            }
-            System.out.println("Enter a value between 1 and 30.");
-        }
-
-        // Period in Years
-        while (true) {
-            System.out.print("Period (Years): ");
-            byte years = scanner.nextByte();
-            if (years >= 1 && years <= 30) {
-                numberOfPayments = years * MONTHS_IN_YEAR;
-                break;
-            }
-            System.out.println("Enter a value between 1 and 30.");
-        }
-
-        double mortgage = principal
-                * (monthlyIR * Math.pow(1 + monthlyIR, numberOfPayments))
-                / (Math.pow(1 + monthlyIR, numberOfPayments) - 1);
-
-        String mortgageCurrency = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println("Mortgage: " + mortgageCurrency);
-
+    public static String greetUser(String firstName, String lastName) {
+        return "Hello " + firstName + " " + lastName;
     }
 }

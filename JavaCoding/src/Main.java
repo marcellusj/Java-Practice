572502504e50783941f9a947c0c8a729915c19a6
Marcellus <majordanduke22@gmail.com>
2022-06-15 08:24:57 -0700

    Refactoring Mortgage Calculator with methods.

--- JavaCoding/src/Main.java
+++ JavaCoding/src/Main.java
@@ -4,12 +4,45 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        String greeting = greetUser("Marcellus", "Jordan");
-        System.out.println(greeting);
 
+        int principal = (int) readNumber("Principal ($1K - $1M): ", 1_000, 1_000_000);
+        float annualIR = (float)readNumber("Annual Interest Rate: ", 1, 30);
+        byte years = (byte)readNumber("Period (Years): ", 1, 30);
+
+        double mortgage = calculateMortgage(principal, annualIR, years);
+
+        String mortgageCurrency = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageCurrency);
     }
 
-    public static String greetUser(String firstName, String lastName) {
-        return "Hello " + firstName + " " + lastName;
+    // Data-validation of input
+    public static double readNumber(String prompt, double min, double max) {
+        Scanner scanner = new Scanner(System.in);
+        double value;
+        while (true) {
+            System.out.print(prompt);
+            value = scanner.nextFloat();
+            if (value >= min && value <= max)
+                break;
+            System.out.println("Enter a value between " + min + " and " + max);
+        }
+        return value;
+    }
+
+    // Calculate mortgage
+    public static double calculateMortgage(int principal,
+                                           float annualIR,
+                                           byte years) {
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        float monthlyIR = annualIR / PERCENT / MONTHS_IN_YEAR;
+        short numberOfPayments = (short)(years * MONTHS_IN_YEAR);
+
+        double mortgage = principal
+                * (monthlyIR * Math.pow(1 + monthlyIR, numberOfPayments))
+                / (Math.pow(1 + monthlyIR, numberOfPayments) - 1);
+
+        return mortgage;
     }
 }
